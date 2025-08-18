a8b0b33b787213edb51811ef2ae30eecf9ef365c
huechologyx <hu.echologyx@gmail.com>
2025-08-18 12:07:37 +0600

    first commit

---
+++ readme-command.py
@@ -0,0 +1,26 @@
+uvicorn app.main:app
+py -3 -m venv venv
+venv\scripts\activate
+venv\scripts\deactivate
+
+
+pip install fastapi[all]
+pip install psycopg2
+pip install sqlalchemy
+pip install passlib[bcrypt]
+pip install python-jose[cryptography]
+
+
+
+https://fastapi.tiangolo.com/tutorial/
+
+https://www.psycopg.org/docs/
+
+https://docs.pydantic.dev/latest/api/base_model/
+
+https://learn.kodekloud.com/user/courses/python-api-development-with-fastapi/module/1c3ad280-5fee-4b8e-a891-42a61f9a2dd0/lesson/3b6e099c-5e15-43f3-944b-0df6aa5f215c
+
+
+
+
+
